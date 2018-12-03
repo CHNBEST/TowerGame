17a7ef2d81eef2061d164dec15732fea5150ace6
SLCTECH\hcao <chnbestfrankcao@hotmail.com>
2018-12-03 16:54:06 -0500

    new changes

--- Tower Defense Chapter 18 Starter/Assets/Scripts/Game/GameManager.cs
+++ Tower Defense Chapter 18 Starter/Assets/Scripts/Game/GameManager.cs
@@ -1,32 +1,4 @@
-﻿/*
- * Copyright (c) 2018 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
- * or sale is expressly withheld.
- *    
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+﻿
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
@@ -62,7 +34,7 @@ public class GameManager : MonoBehaviour
         //2
         if (!gameOver && enemySpawningOver)
         {
-            // Check if no enemies left, if so win game
+            
             //3
             if (EnemyManager.Instance.Enemies.Count == 0)
             {
@@ -70,7 +42,7 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        // When ESC is pressed, quit to the title screen
+        
         //4
         if (Input.GetKeyDown(KeyCode.Escape))
         {
@@ -99,7 +71,7 @@ public class GameManager : MonoBehaviour
 
         if (escapedEnemies == maxAllowedEscapedEnemies)
         {
-            // Too many enemies escaped, you lose the game
+           
             OnGameLose();
         }
     }
--- Tower Defense Chapter 18 Starter/Assets/Scripts/Utility/UtilityMethods.cs
+++ Tower Defense Chapter 18 Starter/Assets/Scripts/Utility/UtilityMethods.cs
@@ -1,42 +1,13 @@
-﻿/*
- * Copyright (c) 2018 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
- * or sale is expressly withheld.
- *    
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
+﻿
 using UnityEngine;
 
 public static class UtilityMethods
 {
-    // Helper method for positioning UI elements to world objects
+
     public static void MoveUiElementToWorldPosition(RectTransform rectTransform, Vector3 worldPosition)
     {
         rectTransform.position = worldPosition + new Vector3(0, 3);
-        rectTransform.LookAt(Camera.main.transform.position + Camera.main.transform.forward * 10000); // Needed to rotate UI the right way
+        rectTransform.LookAt(Camera.main.transform.position + Camera.main.transform.forward * 10000);
         ScaleRectTransformBasedOnDistanceFromCamera(rectTransform);
     }
 
