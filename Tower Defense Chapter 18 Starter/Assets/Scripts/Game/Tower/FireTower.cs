0213ffa219268c738273c3b2eb1a86c6e203187b
SLCTECH\hcao <chnbestfrankcao@hotmail.com>
2018-12-03 16:56:21 -0500

    new changes

--- Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Tower/FireTower.cs
+++ Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Tower/FireTower.cs
@@ -15,8 +15,7 @@ public class FireTower : Tower
         GameObject particles = (GameObject)Instantiate(fireParticlesPrefab, transform.position + new Vector3(0, .5f),
           fireParticlesPrefab.transform.rotation);
         //5
-        particles.transform.localScale *= aggroRadius / 10f; // Scale fire particle radius with the aggro radius
-                                                             //6
+        particles.transform.localScale *= aggroRadius / 10f; 
         foreach (Enemy enemy in EnemyManager.Instance.GetEnemiesInRange(transform.position, aggroRadius))
         {
             enemy.TakeDamage(attackPower);
